bac5070ddbf1ee3d400946444e4df6a017ff4d02
trevormarvin <tmarvin@syntechnix.net>
2018-01-30 20:58:29 -0800

    20180130-2100

--- conflusion/models.py
+++ conflusion/models.py
@@ -13,10 +13,29 @@ from django.contrib.auth.models import User  # for extending the auth table
 
 # This class extends the 'auth_user' table provided by the Django auth package.
 # It contains all the basic information about and set by the user.
-class Users(models.Model):
+class Users(models.User):
+  '''
+  https://docs.djangoproject.com/en/2.0/ref/contrib/auth/#django.contrib.auth.models.User
+  
+  django.contrib.auth.models.User already contains certain fields:
+  
+  username = models.CharField(max_length = 191)
+  first_name = models.CharField(max_length = 30, blank = True)
+  last_name = models.CharField(max_length = 150, blank = True)
+  email = models.EmailField(blank = True)
+  password = models.PASSWORD
+  groups = models.ManyToManyField(models.Group)
+  user_permissions = models.ManyToManyField(models.Permission)
+  is_staff = models.BooleanField()
+  is_active = models.BooleanField()
+  is_superuser = models.BooleanField()
+  last_login = models.DateTimeField()
+  date_joined = models.DateTimeField()
+  '''
+  
   expire = models.DateField(null = True)
   # expiration of all membership priveleges, otherwise use "Certifications" to
-  # tie expirations to particular priveleges
+  # tie expirations of particular priveleges
   
 
 # ---------------- Sites or Resources -----------------------------------------
